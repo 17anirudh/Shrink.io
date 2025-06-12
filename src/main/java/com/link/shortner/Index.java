7fa3f0d4788fd41122f804f0d504e062251a3bf9
Anirudh <vedalaanirudh@gmail.com>
2025-06-12 11:05:47 +0530

    Added a simple index page to the shortner application

---
+++ src/main/java/com/link/shortner/Index.java
@@ -0,0 +1,11 @@
+package com.link.shortner;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Index {
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
--- src/main/java/com/link/shortner/ShortnerApplication.java
+++ src/main/java/com/link/shortner/ShortnerApplication.java
@@ -9,5 +9,4 @@ public class ShortnerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShortnerApplication.class, args);
 	}
-
 }
