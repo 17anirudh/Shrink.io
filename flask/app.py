4d6dd2f644e693556e2b1f658bb83561522dc240
Anirudh <vedalaanirudh@gmail.com>
2025-06-16 21:10:49 +0530

    combined flask and spring-boot in 1 folder

---
+++ flask/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, redirect, render_template
+from mysql.connector import connect
+from mysql.connector.connection import MySQLConnection as MariaDB
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def error():
+    return render_template('error.html')
+
+@app.route('/<string:key>')
+def handle_redirect(key: str):
+    print(key)
+    try:
+        connection = connect(
+            host='localhost',
+            port=3306,
+            database='uri',
+            user='root',
+            password='root@2004'
+        )
+        cursor = connection.cursor()
+        query = 'SELECT link FROM url WHERE pack = %s'
+        cursor.execute(query, (key,))
+        result = cursor.fetchall()
+
+        if result:
+            return redirect(result[0][0])  
+        else:
+            return render_template('error.html')
+
+    except Exception as e:
+        print(f'Exception: {e}')
+        return render_template('error.html')
+    finally:
+        cursor.close()
+        connection.close()
+if __name__ == '__main__':
+    app.run()
