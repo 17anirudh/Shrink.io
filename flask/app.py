25443a547b2c42d1d2d9528c327910457c8e0de5
Anirudh <vedalaanirudh@gmail.com>
2025-06-18 10:01:34 +0530

    some issue with py

--- flask/app.py
+++ flask/app.py
@@ -7,16 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.errorhandler(404)
-def error():
-    print('404')
-    return render_template('error.html')
-
-@app.errorhandler(500)
-def error2():
-    print('500')
-    return render_template('error.html')
-
 @app.route('/<string:key>')
 def handle_redirect(key: str):
     print(key)
@@ -30,14 +20,6 @@ def handle_redirect(key: str):
         )
         print('Connected')
         cursor = connection.cursor()
-        '''cursor.execute("""
-            CREATE TABLE IF NOT EXISTS url (
-                id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                pack VARCHAR(20),
-                link TEXT,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            );
-        """)'''
         query = 'SELECT link FROM url WHERE pack = %s'
         cursor.execute(query, (key,))
         result = cursor.fetchall()
@@ -55,6 +37,18 @@ def handle_redirect(key: str):
             cursor.close()
         if 'connection' in locals():
             connection.close()
+
+
+@app.errorhandler(404)
+def error():
+    print('404')
+    return render_template('error.html')
+
+@app.errorhandler(500)
+def error2():
+    print('500')
+    return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5400)
